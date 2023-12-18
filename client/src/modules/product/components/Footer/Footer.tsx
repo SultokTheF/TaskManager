1a88ccd09cf07a4408b6cb530b1c6d197989754d
SultokTheFe <sultok.003@gmail.com>
2023-12-18 16:16:04 +0100

    New collaborators added

--- client/src/modules/product/components/Footer/Footer.tsx
+++ client/src/modules/product/components/Footer/Footer.tsx
@@ -10,57 +10,55 @@ import Logo from '../../../../assets/images/logo/QUSH_logo_black_expanded.png'
 // import AOS ============================>
 import Aos from 'aos'
 import 'aos/dist/aos.css'
-  
- const Footer: React.FC = () => {
+
+const Footer: React.FC = () => {
   useEffect(()=>{
     Aos.init({duration: 2000})
   }, []) 
 
-   return (
-     <div className='footer' id={`contact`}>
+  return (
+    <div className='footer' id={`contact`}>
       <div className="secCotainer container grid">
-         <div data-aos="fade-up" data-aos-duration="2000" className="logoDiv">
-         <div className="footerLogo">
+        <div data-aos="fade-up" data-aos-duration="2000" className="logoDiv">
+          <div className="footerLogo">
             <a href="#" className="logo flex"><h1>Task Manager</h1></a>
           </div>
+        </div>
 
-          <div  className="socials flex">
-              {/* <ImFacebook className='icon'/>
-              <BsTwitter className='icon'/>
-              <a href='https://www.instagram.com/qush_kz/'><AiFillInstagram className="icon"/></a> */}
-          </div>
-         </div>
-
-         <div data-aos="fade-up" data-aos-duration="2500" className="footerLinks">
+        <div data-aos="fade-up" data-aos-duration="2500" className="footerLinks">
           <span className="linkTitle">Pages</span>
-            <li>
-              <a href="/#main">Home</a>
-            </li>
-            <li>
-              <a href="/#about">About</a>
-            </li>
-          
-         </div>
-         <div data-aos="fade-up" data-aos-duration="3000" className="footerLinks">
+          <li>
+            <a href="/#main">Home</a>
+          </li>
+          <li>
+            <a href="/#about">About</a>
+          </li>
+        </div>
+        <div data-aos="fade-up" data-aos-duration="3000" className="footerLinks">
           <span className="linkTitle">Links</span>
-            <li>
-              <a href="https://github.com/SultokTheF">Sultaniyar</a>
-            </li>
-            <li>
-              <a href="https://github.com/flamemeister">Aldiyar</a>
-            </li>
-            <li>
-              <a href="https://github.com/SultokTheF/TaskManager">Prject URL</a>
-            </li>
-         </div>
-         <div data-aos="fade-up" data-aos-duration="3000" className="footerLinks">
+          <li>
+            <a href="https://github.com/SultokTheF">Sultaniyar</a>
+          </li>
+          <li>
+            <a href="https://github.com/flamemeister">Aldiyar</a>
+          </li>
+          <li>
+            <a href="https://github.com/nurzhanova2">Zarina</a>
+          </li>
+          <li>
+            <a href="https://github.com/samxael">Nuray</a>
+          </li>
+        </div>
+        <div data-aos="fade-up" data-aos-duration="3000" className="footerLinks">
           <span className="linkTitle">Developers</span>
-           <span className='info'>Kuandyk Sultaniyar <span className='email'>220548@astanait.edu.kz</span></span>
-           <span className="info">Saken Aldiyar <span className='email'>220051@astanait.edu.kz</span></span>
-         </div>
+          <span className='info'>Kuandyk Sultaniyar <br /> <span className='email'>220548@astanait.edu.kz</span></span>
+          <span className="info">Saken Aldiyar <br /> <span className='email'>220051@astanait.edu.kz</span></span>
+          <span className="info">Zarina Nurzhanova <br /> <span className='email'>220037@astanait.edu.kz</span></span>
+          <span className="info">Nuray Yelgazy <br /> <span className='email'>220717@astanait.edu.kz</span></span>
+        </div>
       </div> 
-     </div>
-   )
- }
- 
- export default Footer
+    </div>
+  )
+}
+
+export default Footer
